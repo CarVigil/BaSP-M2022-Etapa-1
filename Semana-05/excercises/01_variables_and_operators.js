a6263f42e983f9317b57f8e3c0cfdd22356fc8a3
Carla Daiana Vigil <carvigil21@gmail.com>
2022-04-16 17:21:28 -0300

    BaSP-2022: added missing semicolons and spaces

--- Semana-05/excercises/01_variables_and_operators.js
+++ Semana-05/excercises/01_variables_and_operators.js
@@ -6,20 +6,23 @@
 *  variable.
 *  c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras)
 ************************/
+// ejercicio a
 var num1 = 5;
 var num2 = 3;
 var suma = num1 + num2;
-console.log('\n-Exercise 1.a:')
+console.log('\n-Exercise 1.a:');
 console.log(suma);
 
+// ejercicio b
 var cad1 = 'Hola ';
 var cad2 = 'Mundo';
 var concat = cad1 + cad2;
-console.log('\n-Exercise 1.b:')
+console.log('\n-Exercise 1.b:');
 console.log(concat);
 
+// ejercicio c
 var cad3 = 'Como';
 var cad4 = 'estas';
 var sumaLetras = cad3.length + cad4.length;
-console.log('\n-Exercise 1.c:')
+console.log('\n-Exercise 1.c:');
 console.log(sumaLetras);
