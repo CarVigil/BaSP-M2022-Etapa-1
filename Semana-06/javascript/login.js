5dd805d4f4afff13f1908f316b26d977a6d13a3e
Carla Daiana Vigil <carvigil21@gmail.com>
2022-04-26 14:29:56 -0300

    BaSP-W06-2022: correct validation of alphanumeric pass

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -5,14 +5,14 @@ var errorPass = document.getElementById('error-pass');
 var requiredEmail =  document.getElementById('required-email');
 var requiredPass = document.getElementById('required-pass');
 var re = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
-var alphanumeric = ['a','b','c','d','e','f','g','h',
+var alphabetic = ['a','b','c','d','e','f','g','h',
                     'i','j','k','l','m','n','o','p',
                     'q','r','s','t','u','v','w','x',
                     'y','z','A','B','C','D','E','F',
                     'G','H','I','J','K','L','M','N',
                     'O','P','Q','R','S','T','U','V',
-                    'W','X','Y','Z','0','1','2','3',
-                    '4','5','6','7','8','9'];
+                    'W','X','Y','Z'];
+var numeric = ['0','1','2','3','4','5','6','7','8','9'];
 
 document.getElementById('form').addEventListener('submit', (e) => {
     e.preventDefault();
@@ -44,23 +44,31 @@ function validateEmail(){
             document.getElementById('email').classList.add('valid');
             return true;
         }
-    }
+}
 
 pass.addEventListener('blur', validatePass);
 
 function validatePass(){
     var cont = 0;
+    var letters = 0;
+    var numbers = 0;
     var passLength= pass.value.length;
     for (var i = 0; i < passLength  ; i++){
-        if(!alphanumeric.includes(pass.value[i])){
+        if(!alphabetic.includes(pass.value[i]) && !numeric.includes(pass.value[i])){
             cont++;
+        }
+        if (alphabetic.includes(pass.value[i])){
+            letters++;
+        }
+        if (numeric.includes(pass.value[i])){
+            numbers++;
         }
     }
     if (pass.value === '') {
         requiredPass.style.display = 'block';
         document.getElementById('password').classList.add('error');
         return false;
-    } else if(passLength < 8 || cont != 0){
+    } else if(passLength < 8 || cont != 0 || letters == 0 || numbers == 0){
             errorPass.style.display = 'block';
             document.getElementById('password').classList.add('error');
             return false;
